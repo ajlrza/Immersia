db9236c9293caa1585252506330c1793332a43b4
aj <158788527+ajlrza@users.noreply.github.com>
2026-08-11 17:42:39 +0800

    Refactor engine_request.ts to include new interfaces
    Added dataHashing interface and inMemoryBuffer object to manage rendering and state processes.

--- src/services/engine_request.ts
+++ src/services/engine_request.ts
@@ -1,9 +1,22 @@
 import { actionList, userAction, spriteProperties, loadEngineAPIKey, createEngineRequest } from '../interfaces/engine_interfaces'
 
-const sendRequest(payload: object) {
+export interface dataHashing {
+   hashedObject: any,
+   hashString: string // random
+}
+
+const inMemoryBuffer = {
+    rendering: bytearray = [], 
+    stateProcess: array = [],
+}
+
+const sendRequest(payload: object): void {
     const action: userAction
     action.actionMade = payload.Action // javascript when an event triggered
     action.avatarState = payload.Avatar // already established, filled object in the frontendclient?
     action.generalState = payload.State // general
     action.worldState = payload.World
 }
+
+
+
