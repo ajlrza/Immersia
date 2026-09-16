1c3d16a84b60cef7cdfe2aeab22b357196a69ca9
Tenshi <158788527+basedTenshi@users.noreply.github.com>
2026-09-16 13:53:38 +0800

    feat[llm]: iniatiate draft for llm service and broadcasting mechanism

--- engine/llm.py
+++ engine/llm.py
@@ -1,11 +1,13 @@
 import os, websockets
 from huggingface_hub import HfApi
 from huggingface_hub import login, DeviceCodeError
-from .engine_dc import (
+from engine_dataclasses.main_dc import (
   EnginePayload,
   PromptPayload,
   Data
 )
+
+# thinking of getting inspiration from IPv6 neighbor solicit and broadcasting
 
 # Build a unified middleware service hosted on different soon so if errors occur it wont hit the app
 API_KEY_CATALOG: set[str] = {
@@ -30,7 +32,7 @@ async def api_call(
     first_prompt: PromptPayload = None, 
     api_key: str = None, 
     doRender: bool = False
-    ):
+):
   
   sdk_api: object
   hf_api: object
@@ -48,4 +50,212 @@ async def api_call(
 #  except DeviceCodeError:
 #    return DeviceCodeError
   
+async def openai_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
   
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def anthropic_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+  
+async def google_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def mistral_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def cohere_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def perxplexity_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def openrouter_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def groq_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def together_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def hf_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def deepinfra_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def azure_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def aws_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
+
+async def vertex_call(
+    user_states: EnginePayload = None, 
+    first_prompt: PromptPayload = None, 
+    api_key: str = None, 
+    doRender: bool = False
+):
+  
+  sdk_api: object
+  hf_api: object
+
+  if (doRender):
+    image: bytearray = []
+
+  pass
